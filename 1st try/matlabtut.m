7162b588a8846e4c1b274042b62e727480c34d70
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-08-20 18:33:26 +0430

    Init

---
+++ 1st try/matlabtut.m
@@ -0,0 +1,16 @@
+format compact
+% name = input("What's your name: ", 's');
+% if ~isempty(name)
+%     fprintf("Hello %s\n", name);
+% end
+% 
+% vInput = input("Enter a vector: ");
+% disp(vInput);
+
+% c1 = 'A'
+% class(c1)
+% s1 = 'A String'
+% class(s1)
+% s2 = "A String"
+% class(s2)
+
